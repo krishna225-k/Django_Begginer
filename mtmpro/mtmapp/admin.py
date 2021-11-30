7e780c1cf902e52fb6cea97fa04c6b5f642dcd79
krishna225-k <53045047+krishna225-k@users.noreply.github.com>
2021-11-30 12:36:27 +0530

    Add files via upload

---
+++ mtmpro/mtmapp/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Student,Course
+
+# Register your models here.
+
+# from django.contrib import admin
+# from .models import Student,Course
+
+
+# Register your models here.
+
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['sname', 'location', 'email']
+
+
+class AdminCourse(admin.ModelAdmin):
+    list_display = ['cname', 'fee', 'book']
+
+
+admin.site.register(Student,AdminStudent)
+admin.site.register(Course,AdminCourse)
+
