a6cb5fc4b495cf52f7cfc17976d54d39fde8ffb9
krishna225-k <53045047+krishna225-k@users.noreply.github.com>
2021-11-30 12:56:57 +0530

    Add files via upload

---
+++ realtimeblogpro/realtimeblogapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','author','publish','created','updated','status']
+    list_filter = ('status','created','publish','author')
+    search_fields = ('title','body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    ordering = ['status','publish']
+    
+admin.site.register(Post,PostAdmin)
+
---
+++ realtimeblogpro/realtimeblogapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RealtimeblogappConfig(AppConfig):
+    name = 'realtimeblogapp'
